dad82eceb287a9bf6cb2873b80d1adb33aa4d3aa
Ayad E. Korial <ayad0net@gmail.com>
2021-01-25 14:24:09 +0300

    ver.2.2

--- lib/model.py
+++ lib/model.py
@@ -1,5 +1,13 @@
 from tensorflow.keras.applications.resnet50 import ResNet50
-
+from tensorflow.keras.layers import Dense, Flatten, Dropout, AveragePooling2D
+from tensorflow.keras.models import Model
 def base_model():
-    return ResNet50(weights=None, input_shape=(224,224,3), classes=1)
+    base_model = ResNet50(weights=None, input_shape=(224,224,3),include_top=False, classes=1)
+    headModel = base_model.output
+    headModel = AveragePooling2D(pool_size=(2, 2))(headModel)
+    headModel = Flatten(name="flatten")(headModel)
+    headModel = Dense(256, activation="relu")(headModel)
+    headModel = Dropout(0.5)(headModel)
+    headModel = Dense(1, activation="sigmoid")(headModel)
+    model = Model(inputs=base_model.input, outputs=headModel)
 
